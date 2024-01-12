4114363b7da4bb18d4ae46de0768aae4a83c0a27
aladarahman <aladarahman18@gmail.com>
2024-01-12 05:44:36 +0100

    create models

--- hubs/models.py
+++ hubs/models.py
@@ -1,3 +1,32 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    user_name = models.CharField(max_length=255)
+    email = models.EmailField()
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+class Department(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+
+class Course(models.Model):
+    name = models.IntegerField()
+    course_code = models.TextField()
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+
+class Resource(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    file = models.FileField(upload_to='hubs/files')
+    uploaded_at = models.DateField(auto_now_add=True)
+
+class Comment(models.Model):
+    text = models.TextField()
+    resource = models.ForeignKey(Resource, on_delete=models.CASCADE)
+    commented_by = models.ForeignKey(User, on_delete=models.CASCADE)
